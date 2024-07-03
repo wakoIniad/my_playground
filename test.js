a8b12423bda9e229362d10fd33a2db3a0a63f4ac
wakoIniad <168269931+wakoIniad@users.noreply.github.com>
2024-07-03 13:07:33 +0900

    Update test.js

--- test.js
+++ test.js
@@ -22,23 +22,6 @@ func;
 put (a);
 `
 
-
-const sample3 = `
-#関数もどきの実行例
-sum_1_to_n = {
-#スタックから読み取って指定した名前で保存。
-read(n);
-i = 1;
-result = 0;
-{
-result = result + i;
-i = i + 1;
-} while { !(i >= n) };
-};
-#funcの実行時のスタックに左のオペラントを追加
-put(1 >> func);
-`
-
 const sample4 = `
 #関数もどきの実行例
 sum_1_to_n = {
@@ -55,19 +38,6 @@ result;
 };
 #funcの実行時のスタックに左のオペラントを追加
 put(100 >> sum_1_to_n);
-`
-const test1 = `
-i = 1;
-a = {
-put(i);
-j = 1;
-put(j);
-b = {
-put(j);
-};
-b;
-};
-a;
 `
 const cowaScript = new CowaScript(sample4);
 cowaScript.exe()
